532780eee78fd8df6a4c7ce2d2ec72a990e3ad3c
Sam Zhao <sam@cs.brown.edu>
2015-12-11 17:56:23 -0500

    add cos and sin approximation.

--- simple/simple.cu
+++ simple/simple.cu
@@ -1,21 +1,39 @@
 #include <stdio.h>
 #include <cuda.h>
- 
+
+//__device__ float getSin(float f2)
+//{
+//  return __sin(f2);
+//}
+
 // Kernel that executes on the CUDA device
 __global__ void square_array(float *a, int N)
 {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx<N) a[idx] = a[idx] * a[idx];
 }
- 
-// main routine that executes on the host
-int main(void)
+
+__global__ void sin_array(float *a, int N)
 {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx<N) a[idx] = __sinf(a[idx]);
+}
+
+__global__ void cos_array(float *a, int N)
+{
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx<N) a[idx] = __cosf(a[idx]);
+}
+
+void do_sin() {
+  printf("[info] start sin\n");
   float *a_h, *a_d;  // Pointer to host & device arrays
   const int N = 10;  // Number of elements in arrays
   size_t size = N * sizeof(float);
   a_h = (float *)malloc(size);        // Allocate array on host
   cudaMalloc((void **) &a_d, size);   // Allocate array on device
+
+  printf("[info] prepare data\n");
   // Initialize host array and copy it to CUDA device
   for (int i=0; i<N; i++) a_h[i] = (float)i;
   // Print original results
@@ -25,11 +43,57 @@ int main(void)
   // Do calculation on device:
   int block_size = 4;
   int n_blocks = N/block_size + (N%block_size == 0 ? 0:1);
-  square_array <<< n_blocks, block_size >>> (a_d, N);
+  //square_array <<< n_blocks, block_size >>> (a_d, N);
+  sin_array <<< n_blocks, block_size >>> (a_d, N);
   // Retrieve result from device and store it in host array
   cudaMemcpy(a_h, a_d, sizeof(float)*N, cudaMemcpyDeviceToHost);
+
+  printf("[info] result\n");
   // Print results
   for (int i=0; i<N; i++) printf("%d %f\n", i, a_h[i]);
   // Cleanup
   free(a_h); cudaFree(a_d);
+
+  printf("[info] exit sin\n");
+}
+
+void do_cos() {
+  printf("[info] start cos\n");
+  float *a_h, *a_d;  // Pointer to host & device arrays
+  const int N = 10;  // Number of elements in arrays
+  size_t size = N * sizeof(float);
+  a_h = (float *)malloc(size);        // Allocate array on host
+  cudaMalloc((void **) &a_d, size);   // Allocate array on device
+
+  printf("[info] prepare data\n");
+  // Initialize host array and copy it to CUDA device
+  for (int i=0; i<N; i++) a_h[i] = (float)i;
+  // Print original results
+  for (int i=0; i<N; i++) printf("%d %f\n", i, a_h[i]);
+
+  cudaMemcpy(a_d, a_h, size, cudaMemcpyHostToDevice);
+  // Do calculation on device:
+  int block_size = 4;
+  int n_blocks = N/block_size + (N%block_size == 0 ? 0:1);
+  //square_array <<< n_blocks, block_size >>> (a_d, N);
+  cos_array <<< n_blocks, block_size >>> (a_d, N);
+  // Retrieve result from device and store it in host array
+  cudaMemcpy(a_h, a_d, sizeof(float)*N, cudaMemcpyDeviceToHost);
+
+  printf("[info] result\n");
+  // Print results
+  for (int i=0; i<N; i++) printf("%d %f\n", i, a_h[i]);
+  // Cleanup
+  free(a_h); cudaFree(a_d);
+
+  printf("[info] exit cos\n");
+}
+ 
+// main routine that executes on the host
+int main(void)
+{
+  printf("[info] start main\n");
+  do_sin();
+  do_cos();
+  printf("[info] exit main\n");
 }
